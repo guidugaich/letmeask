f98d60254ecc550df9f2d41e50d8d8571b367c4e
guidugaich <guidugaich.dev@gmail.com>
2021-06-22 12:00:40 -0300

    creating routes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <h1>Hola chicos</h1>
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
