cc7d993e9abb61ed99e4e27a6297bc3e70cecf9d
guidugaich <guidugaich.dev@gmail.com>
2021-06-22 15:22:25 -0300

    Authcontext provider in new file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import { AuthContextProvider } from './contexts/AuthContext';
+
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <AuthContextProvider>
+        <Route exact path="/" component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
