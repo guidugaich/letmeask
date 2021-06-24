859cef37ce3072dcd6e18ec11cacfa44b8c7e1c3
guidugaich <guidugaich.dev@gmail.com>
2021-06-24 15:51:51 -0300

    check if room is closed before joining

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -34,6 +34,11 @@ export function Home() {
       return;
     }
 
+    if (roomRef.val().endedAt) {
+      alert('Room was closed.');
+      return;
+    }
+
     history.push(`/rooms/${roomCode}`)
 
   }
